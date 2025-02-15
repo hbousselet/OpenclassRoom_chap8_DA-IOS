5c2339127b35f537092a8e794c51a692eec777eb
Hugues <hugues.bousselet@gmail.com>
2025-02-15 15:42:37 +0100

    Create DefaultData struct.

--- Arista/Persistence.swift
+++ Arista/Persistence.swift
@@ -48,5 +48,6 @@ struct PersistenceController {
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
+        try! DefaultData(viewContext: container.viewContext).apply()
     }
 }
